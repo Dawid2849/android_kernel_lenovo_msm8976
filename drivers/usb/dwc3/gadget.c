c9a2f7df12b8e0897cd1fefb9a5ffdb2c22b4c69
Dawid2849 <dawid2849@gmail.com>
2024-02-25 23:19:25 +0100

    3.10.85

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -421,31 +421,17 @@ int dwc3_send_gadget_ep_cmd(struct dwc3 *dwc, unsigned ep,
 
 	dwc3_writel(dwc->regs, DWC3_DEPCMD(ep), cmd | DWC3_DEPCMD_CMDACT);
 	do {
-		reg = dwc3_readl(dwc->regs, DWC3_DEPCMD(ep));
-		if (!(reg & DWC3_DEPCMD_CMDACT)) {
+		reg = dwc3_readl(dwc->regs, DWC3_DGCMD);
+		if (!(reg & DWC3_DGCMD_CMDACT)) {
 			dev_vdbg(dwc->dev, "Command Complete --> %d\n",
-					DWC3_DEPCMD_STATUS(reg));
-<<<<<<< HEAD
-			/* SW issues START TRANSFER command to isochronous ep
-			 * with future frame interval. If future interval time
-			 * has already passed when core recieves command, core
-			 * will respond with an error(bit13 in Command complete
-			 * event. Hence return error in this case.
-			 */
-			if (reg & 0x2000)
-				ret = -EAGAIN;
-			else
-				ret = 0;
-			break;
-=======
-			if (DWC3_DEPCMD_STATUS(reg))
+					DWC3_DGCMD_STATUS(reg));
+			if (DWC3_DGCMD_STATUS(reg))
 				return -EINVAL;
 			return 0;
->>>>>>> v3.10.85
 		}
 
 		/*
-		 * We can't sleep here, because it is also called from
+		 * We can't sleep here, because it's also called from
 		 * interrupt context.
 		 */
 		timeout--;
